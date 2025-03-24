5d4c216f1762e1bb78ec556b8329c87bad65ca31
丶曲終人散ゞ <44892049+qzrsa@users.noreply.github.com>
2025-03-24 19:26:54 +0800

    Update diy-script-stable.sh

--- diy-script-stable.sh
+++ diy-script-stable.sh
@@ -54,7 +54,6 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages/tree/
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 
 # Themes
-git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
