3965f0bc2d8f6521f38715f40ae8fd595be64423
丶曲終人散ゞ <44892049+qzrsa@users.noreply.github.com>
2025-03-25 12:33:37 +0800

    Update diy-script-stable.sh

--- diy-script-stable.sh
+++ diy-script-stable.sh
@@ -3,6 +3,9 @@
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 
+# 修改主机名称为 OPENWRT
+sed -i 's/LEDE/OPENWRT/g' package/base-files/files/bin/config_generate
+
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
