b740d84e7177f175366886a10373a2068aff72d7
丶曲終人散ゞ <44892049+qzrsa@users.noreply.github.com>
2025-04-14 17:59:56 +0800

    Update diy-18.06.sh

--- diy-18.06.sh
+++ diy-18.06.sh
@@ -256,8 +256,8 @@ git_clone https://github.com/sbwml/packages_lang_golang golang
 git_clone https://github.com/ximiTech/luci-app-msd_lite
 git_clone https://github.com/ximiTech/msd_lite
 
-clone_all https://github.com/linkease/istore-ui
-clone_all https://github.com/linkease/istore luci
+# clone_all https://github.com/linkease/istore-ui
+# clone_all https://github.com/linkease/istore luci
 
 git_clone main https://github.com/qzrsa/packages luci-app-onliner
 git_clone main https://github.com/qzrsa/packages luci-app-netdata
